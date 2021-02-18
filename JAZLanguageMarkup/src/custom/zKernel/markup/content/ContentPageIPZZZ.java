226278dd1cd47fcfc24ce73c48fe86a54ed5c24c
firak01 <firak01@t-online.de>
2021-02-18 05:49:00 +0100

    Erstelle die Seite
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZLanguageMarkup/src/custom/zKernel/markup/content/ContentPageIPZZZ.java
+++ JAZLanguageMarkup/src/custom/zKernel/markup/content/ContentPageIPZZZ.java
@@ -8,10 +8,12 @@ package custom.zKernel.markup.content;
 
 import org.apache.ecs.HtmlColor;
 import org.apache.ecs.StringElement;
+import org.apache.ecs.html.A;
 import org.apache.ecs.html.BR;
 import org.apache.ecs.html.Font;
 import org.apache.ecs.html.H1;
 import org.apache.ecs.html.HR;
+import org.apache.ecs.html.Input;
 
 import basic.zKernel.IKernelZZZ;
 import basic.zKernel.KernelZZZ;
@@ -76,24 +78,72 @@ public class ContentPageIPZZZ extends ContentEcsZZZ {
 		String sIPTime = this.getVarString("IPTime");
 		StringElement objECS_3b = new StringElement("Datum/Uhrzeit: " + sIPDate + " - " + sIPTime);
 		//this.addElement("StringDateTime", objECS_3b);
-		
-		//org.apache.ecs.html.BR
-		BR objECS_4 = new BR();
-		//this.addElement("BRDateTime", objECS_4);
-		
+			
 		//org.apache.ecs.StringElement
 		String sIPNr = this.getVarString("IPNr");
-		StringElement objECS_4a = new StringElement("IP-Nr: " + sIPNr);
+
+		//StringElement objECS_4a = new StringElement("IP-Nr: " + sIPNr);
 		//this.addElement("IPString",objECS_4a);
 		
-		//F�ge nun die Variable ein. Setze gleichzeitig die Schriftart, etc.				
-		Font objECS_3a = new Font().setSize("-2").setColor(HtmlColor.RED);
-		objECS_3a.addElement(objECS_3b);
-		objECS_3a.addElement(objECS_4);
-		objECS_3a.addElement(objECS_4a);
-		this.addElement("IPDetail", objECS_3a);
-	
+		//Nicht als String, sondern als Link. 
+		//org.apache.ecs.html.A
+		A objECS_4a = new A();
+		objECS_4a.setHref("http://"+sIPNr);
+		objECS_4a.setTagText(sIPNr);		
 		
+		//Zudem noch ein verborgenes Eingabefeld: Darin ist Intern das so wichtige IPNr-Attribut
+		Input objECS_4b = new Input(Input.HIDDEN);
+		objECS_4b.setName("IPNr");
+		objECS_4b.setValue(sIPNr);
+		
+		//Zudem noch ein verborgenes Eingabefeld: Datum
+		Input objECS_4c = new Input(Input.HIDDEN);
+		objECS_4c.setName("IPDate");
+		objECS_4c.setValue(sIPDate);
+		
+		//Zudem noch ein verborgenes Eingabefeld: Zeit
+		Input objECS_4d = new Input(Input.HIDDEN);
+		objECS_4d.setName("IPTime");
+		objECS_4d.setValue(sIPTime);
+		
+		//Fuege nun die Variable ein. Setze gleichzeitig die Schriftart, etc.				
+		Font objECS_3_1 = new Font().setSize("15px").setColor(HtmlColor.RED);
+		objECS_3_1.addElement(objECS_3b);					
+		this.addElement("IPMeta", objECS_3_1);	
+		
+		//org.apache.ecs.html.BR
+		BR objECS_BR1a = new BR();	
+		this.addElement("BR1a",objECS_BR1a);
+		
+		StringElement objECS_x1 = new StringElement("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+		this.addElement("x1",objECS_x1);
+		
+		//org.apache.ecs.html.BR
+		BR objECS_BR1b = new BR();
+		this.addElement("BR1b",objECS_BR1b);
+		
+		
+		
+		Font objECS_3_2 = new Font().setSize("30px").setColor(HtmlColor.BLUE);
+		objECS_3_2.addElement(objECS_4a);
+		this.addElement("IPLink", objECS_3_2);
+		
+		//org.apache.ecs.html.BR
+		BR objECS_BR2a = new BR();	
+		this.addElement("BR2a",objECS_BR2a);
+		
+		StringElement objECS_x2 = new StringElement("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+		this.addElement("x2",objECS_x2);
+		
+		//org.apache.ecs.html.BR
+		BR objECS_BR2b = new BR();
+		this.addElement("BR2b",objECS_BR2b);
+		
+		
+		this.addElement("4b", objECS_4b);
+		this.addElement("4c", objECS_4c);
+		this.addElement("4d", objECS_4d);
+	
 		bReturn = true;
 		}catch(ExceptionZZZ ez){
 			this.objException=ez;
